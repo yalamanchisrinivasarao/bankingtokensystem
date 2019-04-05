603595988914649c8d240e2a74f72201ff14b7b9
yalamanchisrinivasarao <49062073+yalamanchisrinivasarao@users.noreply.github.com>
2019-04-05 12:39:25 +0530

    Add files via upload

--- src/main/java/com/abc/banking/controller/HomeController.java
+++ src/main/java/com/abc/banking/controller/HomeController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
-    
+	
     @Secured({"USER", "ADMIN"})
     @RequestMapping("/")
     public String index() {
-        return "Turvo Banking Token System..";
+    	return  String.format("%s\n%s\n%s\n%s",
+    			"Welcome to the Turvo Banking System." ,
+    		    "For available Counters please click " + "<a href=\"http://localhost:8080/counters\">Counters</a>" ,
+    		    "For available services please click " + "<a href=\"http://localhost:8080/services\">Services</a>" ,
+    		    "Thank You");
     }
 
 }
